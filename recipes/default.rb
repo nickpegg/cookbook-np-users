8bfdf0762cd558349471385139681f19947e9eb1
Nick Pegg <nick@nickpegg.com>
2015-11-08 11:56:48 -0800

    Actually include the dotfiles recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,6 +5,8 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 include_recipe 'user::data_bag'
+
+include_recipe 'np-users::dotfiles'
 include_recipe 'np-users::u2f_keys'
 
 include_recipe 'np-users::nick'
