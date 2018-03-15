831e3bf1727676abfa89bd695d7eae81d9f32ce4
Nick Pegg <nick@nickpegg.com>
2018-03-15 12:19:45 -0700

    Chef 13 compat
    - Remove unused `python` cookbook
    - Bump `user` cookbook to a Chef 13 compatible version
    - Add `apt` dependency which we need

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,8 +4,9 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
+include_recipe 'apt'
+
 include_recipe 'user::data_bag'
 
 include_recipe 'np-users::dotfiles'
-
 include_recipe 'np-users::nick'
