de81d931fff7528d4c932486d5725b8ee38f7dcf
Nick Pegg <nick@nickpegg.com>
2022-05-07 17:30:28 -0700

    Remove dependency on 'user' cookbook (#11)
    * Remove dependency on 'user' cookbook
    
    This cookbook hasn't received any updates in years and has a deprecation
    warning that it'll fail in Chef 18, so just manage users using the
    stdlib resources.

--- metadata.rb
+++ metadata.rb
@@ -3,12 +3,10 @@ maintainer 'Nick Pegg'
 maintainer_email 'nick@nickpegg.com'
 license 'MIT'
 description 'Sets up users'
-version '0.3.2'
+version '0.3.3'
 
 source_url 'https://github.com/nickpegg/cookbook-np-users'
 issues_url 'https://github.com/nickpegg/cookbook-np-users/issues'
-
-depends 'user', '>= 0.6.0'
 
 supports 'arch'
 supports 'debian', '~> 11.0'
--- test/integration/default/default_spec.rb
+++ test/integration/default/default_spec.rb
@@ -1,17 +1,3 @@
-# Check stuff that should have been done by user::data_bag
-describe user('nick') do
-  it { should exist }
-  its('groups') { should include 'nick' }
-  its('groups') { should include 'sudo' }
-  its('groups') { should_not include 'docker' }
-  its('shell') { should eq '/usr/bin/zsh' }
-end
-
-describe file('/home/nick/.ssh/authorized_keys') do
-  its('content') { should match 'nick@polo' }
-  its('content') { should match 'mr8= test key' }
-end
-
 describe user('rsaxvc') do
   it { should exist }
   its('groups') { should include 'rsaxvc' }
